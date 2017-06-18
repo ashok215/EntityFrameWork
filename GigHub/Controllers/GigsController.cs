17d597225797b7d5ad5cdf1e1f110d906d4f4d34
ashok215 <ashokkumarsudha@yahoo.com>
2017-06-18 18:48:43 -0500

    InitialModel

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using GigHub.Models;
+using GigHub.ViewModel;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        public ActionResult Create()
+        {
+            var viewModel = new GigViewModel();
+            viewModel.Genres = _context.Genres.ToList();
+
+            return View(viewModel);
+        }
+    }
+}
